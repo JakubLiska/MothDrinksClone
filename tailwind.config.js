d3d276bb52de5196ff4a0608d579e8da8ed8f21d
Kuba <jakubliska90@gmail.com>
2022-02-08 11:21:42 +0100

    T02

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/app/Pages/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    // fontFamily: {
+    //   poly: ["polysansslim"],
+    // },
+    fontSize: {
+      nav: ["25px", "1.1"],
+    },
+    minWidth: {
+      1440: "1440px",
+    },
+    maxWidth: {
+      1440: "1440px",
+    },
+    extend: {
+      spacing: {
+        "2.4r": "2.4rem",
+      },
+    },
   },
   plugins: [],
 };
